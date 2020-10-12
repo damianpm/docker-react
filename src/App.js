f99f26175d86373d7ca6baa2dc1babff6c37af01
Damian Peralta <damianperaltam@gmail.com>
2020-10-12 17:55:35 +0200

    Feature 2 change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world from master  branch!
+          Hello world from feature-2 branch!
         </p>
         <a
           className="App-link"
