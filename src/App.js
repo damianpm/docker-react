ea7ea8df0afa4548a5289b6ee80085d9c4548fbc
Damian Peralta <damianperaltam@gmail.com>
2020-10-12 17:32:32 +0200

    New change in master

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world from feature  branch!
+          Hello world from master  branch!
         </p>
         <a
           className="App-link"
